0f09f3b06ae1a9e4dbc30fbd195a70919b2027ae
Ivan Ukhov <ivan.ukhov@gmail.com>
2023-01-13 15:45:55 +0100

    Allow draw to take multiple characters

--- founder/src/bin/draw.rs
+++ founder/src/bin/draw.rs
@@ -18,10 +18,10 @@ fn main() {
             return;
         }
     };
-    let character = match arguments.get::<String>("character") {
-        Some(character) => character.chars().next().unwrap(),
+    let characters = match arguments.get::<String>("characters") {
+        Some(characters) => characters.chars().collect(),
         _ => {
-            println!("Error: --character should be given.");
+            println!("Error: --characters should be given.");
             return;
         }
     };
@@ -32,55 +32,69 @@ fn main() {
     founder::scanning::scan_summarize(
         &path,
         process,
-        (character, output),
+        (characters, output),
         arguments.get::<usize>("workers").unwrap_or(1),
         &arguments.get_all::<String>("ignore").unwrap_or(vec![]),
     );
 }
 
-fn process(path: &Path, (character, output): (char, Option<PathBuf>)) -> Result<Option<()>> {
+fn process(path: &Path, (characters, output): (String, Option<PathBuf>)) -> Result<Option<()>> {
     use std::fs::File;
     use std::io::Write;
 
-    match subprocess(path, character) {
-        Ok(Some(result)) => match output {
-            Some(output) => {
-                let output = output.join(path.file_stem().unwrap()).with_extension("svg");
-                let mut file = File::create(output)?;
-                write!(file, "{}", result)?;
-                Ok(Some(()))
+    match subprocess(path, &characters) {
+        Ok(results) => {
+            let mut option = None;
+            for (character, document) in results
+                .into_iter()
+                .filter(|(_, option)| option.is_some())
+                .map(|(character, option)| (character, option.unwrap()))
+            {
+                match output {
+                    Some(ref output) => {
+                        let output = output.join(path.file_stem().unwrap());
+                        std::fs::create_dir_all(&output)?;
+                        let output = output.join(character.to_string()).with_extension("svg");
+                        let mut file = File::create(output)?;
+                        write!(file, "{}", document)?;
+                    }
+                    _ => println!("{}", document),
+                }
+                option = Some(());
             }
-            _ => {
-                println!("{}", result);
-                Ok(Some(()))
-            }
-        },
-        Ok(None) => Ok(None),
+            Ok(option)
+        }
         Err(error) => Err(error),
     }
 }
 
-fn subprocess(path: &Path, character: char) -> Result<Option<element::SVG>> {
+fn subprocess(path: &Path, characters: &str) -> Result<Vec<(char, Option<element::SVG>)>> {
     use font::File;
 
     let File { mut fonts } = File::open(path)?;
     let metrics = fonts[0].metrics()?;
-    let glyph = match fonts[0].draw(character)? {
-        Some(glyph) => glyph,
-        _ => return Ok(None),
-    };
-    let (width, height) = (
-        glyph.width() + 2.0 * glyph.side_bearings.0,
-        metrics.ascender - metrics.descender,
-    );
-    let transform = format!("translate(0, {}) scale(1, -1)", metrics.ascender);
-    let glyph = founder::drawing::draw(&glyph).set("transform", transform);
-    let style = element::Style::new("path { fill: black; fill-rule: nonzero; }");
-    Ok(Some(
-        Document::new()
+    let mut results = vec![];
+    for character in characters.chars() {
+        let glyph = match fonts[0].draw(character)? {
+            Some(glyph) => glyph,
+            _ => {
+                results.push((character, None));
+                continue;
+            }
+        };
+        let (width, height) = (
+            glyph.width() + 2.0 * glyph.side_bearings.0,
+            metrics.ascender - metrics.descender,
+        );
+        let transform = format!("translate(0, {}) scale(1, -1)", metrics.ascender);
+        let glyph = founder::drawing::draw(&glyph).set("transform", transform);
+        let style = element::Style::new("path { fill: black; fill-rule: nonzero; }");
+        let document = Document::new()
             .set("width", width)
             .set("height", height)
             .add(style)
-            .add(glyph),
-    ))
+            .add(glyph);
+        results.push((character, Some(document)));
+    }
+    Ok(results)
 }
